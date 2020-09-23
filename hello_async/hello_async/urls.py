fb670a2949210c370c71ce6ae4c09f9ea0389c08
yyoel <yoelyulius@gmail.com>
2020-09-24 00:30:45 +0700

    #add url path, and new functions

--- hello_async/hello_async/urls.py
+++ hello_async/hello_async/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from hello_async.views import index, async_view, sync_view
+from hello_async.views import index, async_view, sync_view, smoke_some_meats
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('smoke_some_meats/', smoke_some_meats),
     path('async/', async_view),
     path('sync/', sync_view),
     path('', index),
--- hello_async/hello_async/views.py
+++ hello_async/hello_async/views.py
@@ -1,7 +1,9 @@
+from typing import List
 from django.http import HttpResponse
 import asyncio
 import httpx
 from time import sleep
+import random
 
 
 async def http_call_async():
@@ -20,6 +22,52 @@ def http_call_sync():
     r = httpx.get('https://httpbin.org/')
     print(r)
 
+
+async def get_smokables():
+    print('Getting smokeables...')
+
+    await asyncio.sleep(2)
+    async with httpx.AsyncClient() as client:
+        await client.get('https://httpbin.org/')
+
+        print('Returning smokeable')
+        return [
+            'ribs',
+            'brisket',
+            'lemon chicken',
+            'salmon',
+            'bison sirloin',
+            'sausage',
+        ]
+
+
+async def get_flavor():
+    print('Getting flavor...')
+
+    await asyncio.sleep(1)
+    async with httpx.AsyncClient() as client:
+        await client.get('https://httpbin.org/')
+
+        print('Returning flavor')
+        return random.choice (
+            [
+            'Sweet Baby Ray\'s',
+            'Stubb\'s Original',
+            'Famous Dave\'s',
+            ]
+        )
+
+async def smoke(smokeables: List[str] = None, flavor: str = 'Sweet Baby Ray\'s') -> List[str]:
+    """ Smokes some meats and applies the Sweet Baby Ray's """
+
+    for smokeable in smokeables:
+        print(f'Smoking some {smokeable}...')
+        print(f'Applying the {flavor}...')
+        print(f'{smokeable.capitalize()} smoked.')
+
+    return len(smokeables)
+
+
 async def index(request):
     return HttpResponse('Hello, async Django!')
 
@@ -33,3 +81,9 @@ async def async_view(request):
 def sync_view(request):
     http_call_sync()
     return HttpResponse('Blocking HTTP request')
+
+
+async def smoke_some_meats(request):
+    results = await asyncio.gather(*[get_smokables(), get_flavor()])
+    total = await asyncio.gather(*[smoke(results[0], results[1])])
+    return HttpResponse(f'Smoked {total[0]} meats with {results[1]}!')
