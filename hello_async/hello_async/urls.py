7f69ee8f682db7fe794ac8f7ac854190c0879114
yyoel <yoelyulius@gmail.com>
2020-09-23 23:41:56 +0700

    #add new url path

--- hello_async/hello_async/urls.py
+++ hello_async/hello_async/urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('async/', index),
+    path('sync', index),
+    path('', index),
 ]
