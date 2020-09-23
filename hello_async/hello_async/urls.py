c69be3d8d8de00ae45880c713d76daaa28579949
yyoel <yoelyulius@gmail.com>
2020-09-23 23:44:03 +0700

    #mod new url path

--- hello_async/hello_async/urls.py
+++ hello_async/hello_async/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from hello_async.views import index
+from hello_async.views import index, async_view, sync_view
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('async/', index),
-    path('sync', index),
+    path('async/', async_view),
+    path('sync', sync_view),
     path('', index),
 ]
